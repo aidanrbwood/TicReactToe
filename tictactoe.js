d3237b2cd9290d7edf46dc8b012d765567900c47
Aidan Wood <aidanrbwood@gmail.com>
2018-10-31 01:10:08 -0400

    TicReactToe - Basic Implementation, all necessary files here, inital commit

---
+++ tictactoe.js
@@ -0,0 +1,155 @@
+function Again(props) {
+  return (
+  <button
+    className="again"
+    onClick={props.onClick}
+    >
+    {props.value}
+  </button>
+  )
+}
+
+function Square(props) {
+  return (
+  <button 
+    className="square" 
+    onClick={props.onClick}
+    >
+    {props.value ? (props.value == 'X' ? <img src="https://i.imgur.com/HzneJQk.png"></img> : <img src="https://i.imgur.com/4pJL7c0.png"></img>) : ''}
+  </button>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+      isOver: false,
+    });
+  }
+  
+  playAgain(i) {
+    const squares = this.state.squares.slice();
+    squares[0] = null;
+    squares[1] = null;
+    squares[2] = null;
+    squares[3] = null;
+    squares[4] = null;
+    squares[5] = null;
+    squares[6] = null;
+    squares[7] = null;
+    squares[8] = null;
+    this.setState({
+      squares: squares,
+      xIsNext: true,
+      isOver: false,
+    });
+  }
+  
+  renderAgain(i) {
+    return (
+      <Again
+        value={'Play Again'}
+        onClick={() => this.playAgain(i)}
+      />
+    );
+  }
+  
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      this.isOver = true;
+      status = 'Winner:' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'Donut' : 'Carrot');
+    }
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        <div className="again">{this.renderAgain()}</div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a] == 'X' ? " Donut" : "Carrot";
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
